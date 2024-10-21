0b824a3efebcc9475a0a4618fbc34f7e8d19c961
vinimj18 <viniciusmaggiotto@gmail.com>
2024-10-21 20:33:31 +0200

    Day 8

---
+++ day008_camelCasing.py
@@ -0,0 +1,24 @@
+'''Complete the method/function so that it converts dash/underscore delimited 
+words into camel casing. The first word within the output should be capitalized 
+only if the original word was capitalized (known as Upper Camel Case, also often 
+referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"'''
+import re
+
+
+def to_camel_case(text):
+    words = re.split('-|_', text)
+    for i, word in enumerate(words):
+        if i > 0:
+            words[i] = word.capitalize()
+    print(words)
+    return ''.join(words)
+
+
+print(to_camel_case("the-stealth-warrior"))
+print(to_camel_case("The_Stealth_Warrior"))
+print(to_camel_case("The_Stealth-Warrior"))
