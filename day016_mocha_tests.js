81a1495745cd54148c903f10e6fe5a2de543261d
vinimj18 <viniciusmaggiotto@gmail.com>
2024-10-29 11:36:52 +0100

    Day 16

---
+++ day016_mocha_tests.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+
+describe("tests to train my Mocha learning", function () {
+  let input = undefined;
+
+  beforeEach(function () {
+    input = Math.floor(Math.random() * 100);
+    console.log(input);
+  });
+
+  it("Checks if input is even", function () {
+    const isEven = input % 2 === 0;
+    assert.equal(isEven, true);
+  });
+  it.skip("Checks if input is divisible by 3", function () {
+    const isEven = input % 3 === 0;
+    assert.equal(isEven, true);
+  });
+  it(
+    "Check if the square root of input multiplied by 5! is divisible by pi (TO BE IMPLEMENTED) XD"
+  );
+});
