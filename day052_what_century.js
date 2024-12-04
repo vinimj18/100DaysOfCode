d5f8c9b23156355c8963a7af524cf947af32f0a8
vinimj18 <viniciusmaggiotto@gmail.com>
2024-12-04 15:18:54 +0100

    Day 52

---
+++ day052_what_century.js
@@ -0,0 +1,19 @@
+function whatCentury(year) {
+  // Calculating the century
+  let century = 0;
+  if (year % 100 === 0) century = Math.floor(year / 100);
+  else century = Math.floor(year / 100 + 1);
+
+  // Building the ordinal string
+  const sufix = ["th", "st", "nd", "rd"];
+  // let value = century % 100;
+  return century + (sufix[(century - 20) % 10] || sufix[century] || sufix[0]);
+}
+
+console.log(whatCentury("1999"), "20th");
+console.log(whatCentury("2011"), "21st");
+console.log(whatCentury("2154"), "22nd");
+console.log(whatCentury("2259"), "23rd");
+console.log(whatCentury("1234"), "13th");
+console.log(whatCentury("1023"), "11th");
+console.log(whatCentury("2000"), "20th");
